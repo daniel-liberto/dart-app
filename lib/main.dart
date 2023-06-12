058c3e7f4154f5690aced83cd0eec0b23a21d47e
daniel-liberto <daniel.liberto@hotmail.com>
2023-06-12 17:33:47 -0300

    style and structure lesson

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   //runApp(const MyApp());
-  runApp(
-    const MaterialApp(
-      home: Center(
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: const Text('App Hello World')),
+      body: const Center(
         child: Text(
           'Hello World!',
-          style: TextStyle(fontSize: 50, color: Colors.white),
+          style: TextStyle(fontSize: 50, color: Colors.black),
         ),
       ),
     ),
-  );
+  ));
 }
