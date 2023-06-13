8b9532ca781cf8f98e968338eb9f547cd3edbf4f
daniel-liberto <daniel.liberto@hotmail.com>
2023-06-13 17:05:06 -0300

    statelessWidget lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const MyApp());
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: const Text('App Hello World')),
-      body: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(fontSize: 50, color: Colors.black),
+  runApp(const MyApp());
+  // runApp(MaterialApp(
+  //   home: Scaffold(
+  //     appBar: AppBar(title: const Text('App Hello World')),
+  //     body: const Center(
+  //       child: Text(
+  //         'Hello World!',
+  //         style: TextStyle(fontSize: 50, color: Colors.black),
+  //       ),
+  //     ),
+  //   ),
+  // ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('App Hello World')),
+        body: const Center(
+          child: Text(
+            'Hello World!',
+            style: TextStyle(fontSize: 50, color: Colors.black),
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
